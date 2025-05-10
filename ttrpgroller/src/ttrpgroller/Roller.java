64d7bd30dabf06296a9e0ee1c676cd5b02e4006c
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-10 14:41:53 -0400

    init commit

---
+++ ttrpgroller/src/ttrpgroller/Roller.java
@@ -0,0 +1,14 @@
+package ttrpgroller;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Roller {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		
+	}
+
+}
